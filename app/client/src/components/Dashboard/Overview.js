9b755d35a53d80052dec5c9a85328517f26512a2
adityajadhav10 <adityawestin@gmail.com>
2019-12-08 19:16:17 +0100

    Overview Screen : Cards structure

--- app/client/src/components/Dashboard/Overview.js
+++ app/client/src/components/Dashboard/Overview.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import './../Styles/common.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#312551',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+
+}));
 
 const Overview = () => {
+    const classes = useStyles();
     return (
-        <div>
-            Overview Page
+        <div className={classes.root}>
+            <div className='alignMargin'>
+                <Grid container spacing={2} style={{height:'100%'}}>
+                <Grid item  xs={5}>
+                    <Paper className={classes.paper} style={{height:'99%'}}>View Team</Paper>
+                    <Paper className="cardTail" />
+                </Grid>
+                <Grid item xs>
+                    <Paper className={classes.paper} style={{height:'48%'}}>Fixtures</Paper>
+                    <Paper className="cardTail" style={{marginBottom : '12px'}}/>
+                    <Paper className={classes.paper} style={{height:'48%'}}>Points</Paper>
+                    <Paper className="cardTail" />
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper} style={{height:'99%'}}>Standings</Paper>
+                    <Paper className="cardTail" />
+                </Grid>
+                </Grid>
+            </div>      
         </div>
     )
 }
